99e53e5560f6429f02f5517d3840a18b3c84695a
ashish <ashish.mehta@process9.com>
2025-04-29 16:46:37 +0530

    commit program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+//create a ElectronicItem class and initialize with some properties
+public class ElectronicItem
+{
+    public string Name { get; set; }
+    public string Brand { get; set; }
+    public double Price { get; set; }
+    public int WarrantyPeriod { get; set; } // in months
+    public ElectronicItem(string name, string brand, double price, int warrantyPeriod)
+    {
+        Name = name;
+        Brand = brand;
+        Price = price;
+        WarrantyPeriod = warrantyPeriod;
+    }
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Name: {Name}, Brand: {Brand}, Price: {Price}, Warranty Period: {WarrantyPeriod} months");
+    }
+}
